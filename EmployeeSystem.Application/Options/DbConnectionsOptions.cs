a4d63ef323544ad4d54d73222d474e34f22326a6
bekirov <vbekirovv@gmail.com>
2022-12-11 21:19:32 +0200

    Added db connection options.

---
+++ EmployeeSystem.Application/Options/DbConnectionsOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeSystem.Application.Options;
+
+public class DbConnectionsOptions
+{
+    public const string SectionName = "ConnectionStrings";
+
+    public string SystemDb { get; set; } = string.Empty;
+}
