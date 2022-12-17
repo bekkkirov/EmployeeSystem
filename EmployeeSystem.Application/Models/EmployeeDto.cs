9b0af3b57fc31ade1e80725ebbb774c8504e3da2
bekirov <vbekirovv@gmail.com>
2022-12-17 10:34:55 +0200

    Added business models.

---
+++ EmployeeSystem.Application/Models/EmployeeDto.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeSystem.Domain.Entities;
+
+namespace EmployeeSystem.Application.Models;
+
+public class EmployeeDto
+{
+    public string FirstName { get; set; } = default!;
+
+    public string LastName { get; set; } = default!;
+
+    public string UserName { get; set; } = default!;
+
+    public string Email { get; set; } = default!;
+
+    public DateTime BirthDate { get; set; }
+
+    public decimal Salary { get; set; }
+
+    public Post Post { get; set; }
+
+    public List<TaskDto> AssignedTasks { get; set; } = new List<TaskDto>();
+}
---
+++ EmployeeSystem.Application/Models/TaskDto.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeSystem.Domain.Entities;
+using TaskStatus = EmployeeSystem.Domain.Entities.TaskStatus;
+
+namespace EmployeeSystem.Application.Models;
+
+public class TaskDto
+{
+    public string Description { get; set; } = default!;
+
+    public DateTime Created { get; set; }
+
+    public DateTime CompleteBy { get; set; }
+
+    public TaskStatus Status { get; set; }
+}
