ef09947c7039bc3f883d358a84d8820bd4cc9dbc
bekirov <vbekirovv@gmail.com>
2022-12-17 11:53:27 +0200

    Implemented required controllers.

---
+++ EmployeeSystem.API/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using EmployeeSystem.Application.Interfaces.Services;
+using EmployeeSystem.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeSystem.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly IEmployeeService _employeeService;
+
+    public EmployeesController(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+
+    [HttpGet("current")]
+    public async Task<ActionResult<EmployeeDto>> GetCurrentProfile()
+    {
+        var employee = await _employeeService.GetCurrentProfileAsync();
+
+        return Ok(employee);
+    }
+
+    [HttpGet("{userName}")]
+    public async Task<ActionResult<EmployeeDto>> GetEmployeeProfile(string userName)
+    {
+        var employee = await _employeeService.GetEmployeeProfileAsync(userName);
+
+        return Ok(employee);
+    }
+
+}
---
+++ EmployeeSystem.API/Controllers/TasksController.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeSystem.Application.Interfaces.Services;
+using EmployeeSystem.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeSystem.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TasksController : ControllerBase
+{
+    private readonly ITaskService _taskService;
+
+    public TasksController(ITaskService taskService)
+    {
+        _taskService = taskService;
+    }
+
+    [HttpGet("current")]
+    public async Task<ActionResult<IEnumerable<TaskDto>>> GetCurrentEmployeeTasks()
+    {
+        var tasks = await _taskService.GetCurrentTasksAsync();
+
+        return Ok(tasks);
+    }
+
+    [HttpGet("{userName}")]
+    public async Task<ActionResult<IEnumerable<TaskDto>>> GetEmployeeTasks(string userName)
+    {
+        var tasks = await _taskService.GetEmployeeTasksAsync(userName);
+
+        return Ok(tasks);
+    }
+}
