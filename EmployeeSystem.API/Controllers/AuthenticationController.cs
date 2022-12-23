b4678adaa5c58119e9da4cf7dcc2170a84c91f2f
bekirov <vbekirovv@gmail.com>
2022-12-23 13:53:31 +0200

    Implemented authentication controller.

---
+++ EmployeeSystem.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeSystem.Application.Interfaces.Services;
+using EmployeeSystem.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeSystem.API.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    [HttpPost]
+    [Route("sign-in")]
+    public async Task<ActionResult<string>> SignIn(SignInDto signInData)
+    {
+        var result = await _authenticationService.SignInAsync(signInData);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Route("sign-up")]
+    public async Task<ActionResult<string>> SignUp(SignUpDto signUpData)
+    {
+        var result = await _authenticationService.SignUpAsync(signUpData);
+
+        return Ok(result);
+    }
+}
